1b1a8fd583c2bb135e75766576e27ba6590fcdba
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-08-21 21:42:17 +0500

    Completed

---
+++ section6/magic1.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __new__(cls):
+        print ("__new__ magic method is called")
+        inst = object.__new__(cls)
+        return inst
+    def __init__(self):
+        print ("__init__ magic method is called")
+        self.name='Satya'
+        return self.name
